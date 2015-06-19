2f106d9de27622e2a4c6e08f7c6e8832582168dc
Rafael Dohms <rdohms@gmail.com>
2015-06-19 22:37:37 +0200

    Reverted API call location
    Since symposium runs in HTTP, calls from HTTPS pages, like google form are blocked from within the page.
    Moving the call back to the popup allows it to be made regardless of context and then pass the data for filling.

--- src/js/popup/TalkLister.js
+++ src/js/popup/TalkLister.js
@@ -77,17 +77,24 @@ class TalkLister {
         );
     }
 
-    initiateFormFill(e) {
-
-        let talk_id = $(e.currentTarget).data("talkId");
-
+    _sendTalkData(data) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {type: "FILL_TALK", talk_id: talk_id}, function(response) {
+            chrome.tabs.sendMessage(tabs[0].id, {type: "FILL_TALK", talk_data: data}, function(response) {
                 if (response.success) {
                     window.close();
                 }
             });
         });
+    }
+
+    initiateFormFill(e) {
+
+        let talk_id = $(e.currentTarget).data("talkId");
+
+        this.api.getTalkInfo(talk_id, talk => {
+            this._sendTalkData(talk.attributes);
+        });
+
     }
 
 }
--- src/js/pronto-popup.js
+++ src/js/pronto-popup.js
@@ -1,4 +1,7 @@
 import {TalkLister} from "./popup/TalkLister";
 import {SymposiumApiClient} from "./api/SymposiumApiClient";
 
+// let jQuery = require('jquery');
+// require('../../build/js/bootstrap.min.js');
+
 let pronto = new TalkLister(new SymposiumApiClient());
